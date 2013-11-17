64a79ba58cb84c17648446d6235617c784aab9cf
mpescarmona <mpescarmona@gmail.com>
2013-11-17 17:19:42 -0300

    Adding UserService into controller inner logics.-

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -3,43 +3,70 @@
 /* Controllers */
 
 angular.module('brandscopicApp.controllers', [])
-  .controller('LoginController', ['$scope', '$state', function($scope, $state) {
-    $scope.validUser = false;
+  .controller('LoginController', ['$scope', '$state', 'UserService', function($scope, $state, UserService) {
     $scope.user = {'email': '', 'password': ''};
 
     $scope.users = [{'email': 'testuser@brandscopic.com', 'password': 'testuser'},
                     {'email': 'testuser1@brandscopic.com', 'password': 'testuser1'}];
 
     $scope.validateUser = function() {
-     $scope.validUser = false;
+      UserService.currentUser.isLogged = false;
+      UserService.currentUser.email = "";
         for (var i = 0, u, sameEmail, samePasswd; u = $scope.users[i++];) {
           sameEmail = u.email.toLowerCase() == $scope.user.email.toLowerCase();
           samePasswd = u.password == $scope.user.password;
           console.log(sameEmail)
           if (sameEmail && samePasswd) {
-           $scope.validUser = true;
-            console.log($scope.validUser);
+            UserService.currentUser.isLogged = true;
+            UserService.currentUser.email = u.email.toLowerCase();
+            console.log(UserService);
             $state.go('home');
-            return
+            return;
           }
         }   
     };
-
   }])
-  .controller('HomeController', ['$scope', 'snapRemote',  function($scope, snapRemote ) {
+  .controller('HomeController', ['$scope', '$state', 'snapRemote', 'UserService',  function($scope, $state, snapRemote, UserService) {
+    if( !UserService.isLogged() ) {
+      $state.go('login');
+      return;
+    }
+
+    $scope.logout = function() {
+      UserService.currentUser.isLogged = false;
+      UserService.currentUser.email = "";
+      $state.go('login');
+      return;
+    };
+
     $scope.navigationItems = [{'class': 'eventIcon', 'label': 'EVENTS', 'link': '#'},
                               {'class': 'tasksIcon', 'label': 'TASKS',  'link': '#'},
                               {'class': 'venuesIcon', 'label': 'VENUES', 'link': '#'},
                               {'class': 'notificationIcon', 'label': 'NOTIFICATIONS', 'link': '#'},
                               {'class': 'dashboardIcon', 'label': 'DASHBOARD', 'link': '#home/dashboard'}];
 
-    $scope.actionItems = [{'class': 'profileIcon', 'label': 'EDIT PROFILE', 'link': '#'},
-                          {'class': 'logoutIcon', 'label': 'LOGOUT', 'link': '#/login'}];
+    $scope.actionItems = [{'class': 'profileIcon', 'label': 'EDIT PROFILE', 'link': '#', 'click': ''},
+                          {'class': 'logoutIcon', 'label': 'LOGOUT', 'link': '#', 'click': 'logout()'}];
   }])
-  .controller('DashboardController', ['$scope', 'snapRemote', function($scope, snapRemote) {
-    snapRemote.close()
+  .controller('DashboardController', ['$scope', '$state', 'snapRemote', 'UserService',  function($scope, $state, snapRemote, UserService) {
+    if( !UserService.isLogged() ) {
+      $state.go('login');
+      return;
+    }
+    snapRemote.close();
+
+    $scope.dashboardItems = [{'id': 1, 'name': 'Gin BAs FY14', 'today': '30%', 'progress': '40%'},
+                             {'id': 2, 'name': 'Jameson Locals FY14', 'today': '65%', 'progress': '10%'},
+                             {'id': 3, 'name': 'Jameson CE FY13', 'today': '75%', 'progress': '60%'},
+                             {'id': 4, 'name': 'Gin BAs FY14', 'today': '45%', 'progress': '80%'},
+                             {'id': 5, 'name': 'Mama Walker\'s FY14', 'today': '65%', 'progress': '30%'},
+                             {'id': 6, 'name': 'Royal Salute FY14', 'today': '25%', 'progress': '30%'}];
   }])
-  .controller('EventsController', ['$scope', function($scope, snapRemote) {
+  .controller('EventsController', ['$scope', '$state', 'snapRemote', 'UserService',  function($scope, $state, snapRemote, UserService) {
+    if( !UserService.isLogged() ) {
+      $state.go('login');
+      return;
+    }
     snapRemote.close()
   }])
   .controller('PasswordController', ['$scope', function($scope) {
