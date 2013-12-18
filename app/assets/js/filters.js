de97e10697ed906328956dadcabc51149bd7e633
mpescarmona <mpescarmona@gmail.com>
2013-12-18 20:24:42 -0300

    Allow show all filter buttons. Change total number format. Issues #34

--- app/assets/js/filters.js
+++ app/assets/js/filters.js
@@ -2,9 +2,19 @@
 
 /* Filters */
 
-angular.module('brandscopicApp.filters', []).
-  filter('interpolate', ['version', function(version) {
+angular.module('brandscopicApp.filters', [])
+  .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+
+  .filter('totalCount', function() {
+    return function(input) {
+      if(input > 1000) {
+        var result = (input / 1000).toFixed(1).toString() + "K";
+        return result;
+      }
+      return input;
+    }
+  });
