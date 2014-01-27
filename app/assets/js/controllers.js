9b0f4696ec96e8c534cece1b7e1b14c7424d4468
mpescarmona <mpescarmona@gmail.com>
2014-01-26 22:24:27 -0300

    Allowing Add Team members to Event. Add abstraction layer for Teams. Add new Add Team Partial page. Issue #65

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', 'model.expense', 'model.comment', 'model.eventContact', 'model.contact', 'model.country'])
+angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', 'model.expense', 'model.comment', 'model.eventContact', 'model.eventTeam', 'model.contact', 'model.country'])
   .controller('MainController', ['$scope', 'UserService', function($scope, UserService) {
     $scope.UserService = UserService;
   }])
@@ -519,7 +519,7 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
 
       , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
       , actions = { success: function(event) {
-                                    $scope.event = event;
+                                    $scope.event = event
 
                                     // Options for User Interface in home partial
                                     ui.title = event.campaign ? event.campaign.name : "People"
@@ -528,45 +528,44 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
                                     $scope.UserInterface = UserInterface
 
                                     $scope.showPeople = "team"
+                                    $scope.UserInterface.AddIconState = "home.events.details.people.team.add";
                                     $scope.showPeopleType = function(type) {
                                       $scope.showPeople = type
+                                      if ($scope.showPeople =="team")
+                                        $scope.UserInterface.AddIconState = "home.events.details.people.team.add"
+                                      if ($scope.showPeople =="contacts")
+                                        $scope.UserInterface.AddIconState = "home.events.details.people.contacts.add"
                                     }
-
-                                    // if ($scope.showPeople =="team") {
-                                    //   $scope.UserInterface.AddIconState = "home.events.details.people.team.add";
-                                    // } else {
-                                      $scope.UserInterface.AddIconState = "home.events.details.people.contacts.add";
-                                    // }
 
                                     // get event Team members
                                     promiseTeam.then(function(responseTeam) {
                                      if (responseTeam.status == 200) {
                                       if (responseTeam.data != null) {
-                                          eventTeamData = responseTeam.data;
-                                          $scope.eventTeamItems = eventTeamData;
+                                          eventTeamData = responseTeam.data
+                                          $scope.eventTeamItems = eventTeamData
                                       }
                                      } else {
-                                        $scope.eventTeamItems = {};
+                                        $scope.eventTeamItems = {}
                                      }
-                                    });
+                                    })
                                     promiseTeam.catch(function(responseTeam) {
-                                      $scope.eventTeamItems = {};
-                                    });
+                                      $scope.eventTeamItems = {}
+                                    })
 
                                     // get event Contact members
                                     promiseContacts.then(function(responseContacts) {
                                      if (responseContacts.status == 200) {
                                       if (responseContacts.data != null) {
-                                          eventContactsData = responseContacts.data;
-                                          $scope.eventContactItems = eventContactsData;
+                                          eventContactsData = responseContacts.data
+                                          $scope.eventContactItems = eventContactsData
                                       }
                                      } else {
-                                        $scope.eventContactItems = {};
+                                        $scope.eventContactItems = {}
                                      }
-                                    });
+                                    })
                                     promiseTeam.catch(function(responseTeam) {
-                                      $scope.eventContactItems = {};
-                                    });
+                                      $scope.eventContactItems = {}
+                                    })
                              }
         }
 
@@ -816,6 +815,59 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
       Contact.create(credentials, actions, $scope.contact)
     }
 
+  }])
+
+  .controller('EventsPeopleTeamAddController', ['$scope', '$state', '$location', '$stateParams', 'snapRemote', 'UserService', 'CompanyService', 'UserInterface', 'Event', 'EventTeam', function($scope, $state, $location, $stateParams, snapRemote, UserService, CompanyService, UserInterface, Event, EventTeam) {
+    if( !UserService.isLogged() ) {
+      $state.go('login');
+      return;
+    }
+    snapRemote.close()
+
+    var
+        ui = {title: "Event team", hasMenuIcon: false, hasDeleteIcon: true, hasBackIcon: false, hasMagnifierIcon: false, hasAddIcon: false, hasEditIcon: false, hasSaveIcon: false, hasCancelIcon: false, hasCloseIcon: false, showEventSubNav: false, hasCustomHomeClass: false, searching: false, eventSubNav: "people"}
+      , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
+      , actions = { success: function(event) {
+                                    $scope.event = event;
+                                    $scope.eventId = $stateParams.eventId
+
+                                    // Options for User Interface in home partial
+                                    angular.extend(UserInterface, ui)
+                                    $scope.UserInterface = UserInterface
+
+                                    var
+                                        credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
+                                      , actions = { success: function(teams) {
+                                                                  $scope.teams = teams
+
+                                                                  $scope.assignTeam = function(teamId, teamType) {
+                                                                    var
+                                                                        credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId, memberable_id: teamId, memberable_type: teamType }
+                                                                      , actions = { success: function (contact) {
+                                                                                      // workaround for remove the non 'Active' events
+                                                                                      for(var i = 0, item; item = $scope.teams[i++];) {
+                                                                                        if (item.id == teamId) {
+                                                                                          $scope.teams.splice(i-1, 1)
+                                                                                        }
+                                                                                      }
+
+                                                                                      $location.path("/home/events/" + $scope.event.id + "/people/team/add")
+                                                                                    }
+                                                                                  , error: function (team_error) {
+                                                                                      $scope.team_error = team_error
+                                                                                       console.log(team_error)
+                                                                                    }
+                                                                                  }
+
+                                                                    EventTeam.create(credentials, actions, $scope.event)
+                                                                  }
+                                                             }
+                                      }
+                                    EventTeam.members(credentials, actions)
+                    }
+        }
+
+   Event.find(credentials, actions)
   }])
 
   .controller('EventsDataController', ['$scope', '$state', '$stateParams', 'snapRemote', 'UserService', 'CompanyService','UserInterface', 'Event', 'EventsRestClient', function($scope, $state, $stateParams, snapRemote, UserService, CompanyService, UserInterface, Event, EventsRestClient) {
