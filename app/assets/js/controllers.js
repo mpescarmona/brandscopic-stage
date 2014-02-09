e960c20428486de38656545cf38f622851e10750
mpescarmona <mpescarmona@gmail.com>
2014-02-09 19:04:58 -0300

    Allowing update Event Data Results. Issue #69

--- app/assets/js/controllers.js
+++ app/assets/js/controllers.js
@@ -857,7 +857,7 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
    Event.find(credentials, actions)
   }])
 
-  .controller('EventsDataController', ['$scope', '$state', '$stateParams', 'snapRemote', 'UserService', 'CompanyService','UserInterface', 'Event', 'EventsRestClient', function($scope, $state, $stateParams, snapRemote, UserService, CompanyService, UserInterface, Event, EventsRestClient) {
+  .controller('EventsDataController', ['$scope', '$state', '$stateParams', '$location', 'snapRemote', 'UserService', 'CompanyService','UserInterface', 'Event', function($scope, $state, $stateParams, $location, snapRemote, UserService, CompanyService, UserInterface, Event) {
     if( !UserService.isLogged() ) {
       $state.go('login');
       return;
@@ -871,9 +871,6 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
       , companyId = CompanyService.getCompanyId()
       , eventId = $stateParams.eventId
       , eventResultsData = []
-      , eventResults = new EventsRestClient.getEventResultsById(authToken, companyId, eventId)
-      , promiseResults = eventResults.getEventResultsById().$promise
-
       , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
       , actions = { success: function(event) {
                                     $scope.event = event
@@ -887,24 +884,89 @@ angular.module('brandscopicApp.controllers', ['model.event', 'model.campaign', '
                                         credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
                                       , actions = { success: function(results) {
                                                                 $scope.eventResultsItems = results
-                                                                // $scope.impressions = {}
-                                                                // $scope.interactions = {}
-                                                                // $scope.samples = {}
-                                                                // for(var i = 0, item; item = results[i++];) {
-                                                                //   if (item.name=='Impressions')
-                                                                //     $scope.impressions = item
-                                                                //   if (item.name=='Interactions')
-                                                                //     $scope.interactions = item
-                                                                //   if (item.name=='Samples')
-                                                                //     $scope.samples = item
-                                                                // }
-
                                                              }
                                       }
                                     Event.results(credentials, actions)
                     }
         }
    Event.find(credentials, actions)
+
+    $scope.updateEventData = function(results) {
+      var
+          credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
+        , actions = { success: function (event) {
+                            $scope.event = event
+                            $location.path("/home/events/" + event.id + "/data/view")
+                      }
+                    , error: function (event_error) {
+                        $scope.event_error = event_error
+                         console.log(event_error)
+                      }
+                    }
+        // , results_attributes = {}
+        , results_attributes = []
+        // , idx = 0
+        // , eventResults = []
+
+      for(var i = 0, result; result = results[i++];) {
+        for(var j = 0, field; field = result.fields[j++];) {
+          if (field.field_type == 'percentage') {
+            for(var k = 0, segment; segment = field.segments[k++];) {
+              if (segment.value !== null)
+                results_attributes.push({'id': segment.id, 'value': segment.value})
+            }
+          }
+          if (field.field_type == 'number') {
+            if (field.value !== null)
+              results_attributes.push({'id': field.id, 'value': field.value})
+          }
+          if (field.field_type == 'text') {
+            if (field.value !== null)
+              results_attributes.push({'id': field.id, 'value': field.value})
+          }
+          if (field.field_type == 'count') {
+            if (field.options.capture_mechanism == "checkbox") {
+              var segmentIds = []
+              for(var k = 0, segment; segment = field.segments[k++];) {
+                if (segment.value !== null)
+                  segmentIds.push(segment.id)
+              }
+              if (segmentIds.length > 0)
+                results_attributes.push({'id': field.id, 'value': segmentIds})
+            } else {
+              for(var k = 0, segment; segment = field.segments[k++];) {
+                if (segment.value !== null) {
+                  results_attributes.push({'id': field.id, 'value': segment.id})
+                  break                        
+                }
+              }
+            }
+          }
+        }
+      }
+
+      // var testData = {
+      //               "event": {
+      //                 "summary": "Test Summary",
+      //                 "results_attributes": [
+      //                   {"id": "67816", "value": "750"}
+      //                 ]
+      //               }
+      //             }
+
+      var data = {
+                    "event": {
+                      "summary": $scope.event.summary,
+                      "results_attributes": results_attributes
+                    }
+                  }
+
+
+      // Event.updateResults(credentials, actions, testData)
+      Event.updateResults(credentials, actions, data)
+    }
+
+
 
   }])
 
--- app/assets/js/persistence/event.js
+++ app/assets/js/persistence/event.js
@@ -2,28 +2,33 @@ angular.module('persistence.event', ['ngResource', 'util.jsonToFormData'])
 
 .factory('eventClient', ['$resource', 'jsonToFormDataFor', function($resource, jsonToFormDataFor) {
   var contentType = { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}
+    , contentTypeJson = { 'Accept': 'application/json', 'Content-Type': 'application/json'}
 
   return $resource('//stage.brandscopic.com/api/v1/events/:event_id.:format', {auth_token: '@token', format: 'json', company_id: '@company_id'},
   {
-        'all'     : { method: 'GET' }
+        'all'           : { method: 'GET' }
 
-      , 'find'    : { method: 'GET' }
+      , 'find'          : { method: 'GET' }
 
-      , 'create'  : { method: 'POST'
-                     , headers: contentType
-                     , transformRequest: jsonToFormDataFor('event')
-                    }
+      , 'create'        : { method: 'POST'
+                           , headers: contentType
+                           , transformRequest: jsonToFormDataFor('event')
+                          }
 
-      , 'update'    : { method: 'PUT'
-                     , headers: contentType
-                     , transformRequest: jsonToFormDataFor('event')
-                    }
+      , 'update'        : { method: 'PUT'
+                           , headers: contentType
+                           , transformRequest: jsonToFormDataFor('event')
+                          }
 
-      , 'search'     : { method: 'GET', params: {action:'autocomplete'} }
+      , 'updateResults' : { method: 'PUT'
+                           , headers: contentTypeJson
+                          }
 
-      , 'results': { method: 'GET'
-                      , isArray: true 
-                      , url: '//stage.brandscopic.com/api/v1/events/:event_id/results.:format'
-                    }
+      , 'search'        : { method: 'GET', params: {action:'autocomplete'} }
+
+      , 'results'       : { method: 'GET'
+                            , isArray: true 
+                            , url: '//stage.brandscopic.com/api/v1/events/:event_id/results.:format'
+                          }
   });
 }]);
