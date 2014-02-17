ee69153c63427acd7f27404acaa65606ec4ae664
mpescarmona <mpescarmona@gmail.com>
2014-02-17 01:00:16 -0300

    Filtering events list for 'Active' and future events only. Issue #38

--- app/assets/js/controllers/events/eventsController.js
+++ app/assets/js/controllers/events/eventsController.js
@@ -9,15 +9,25 @@ function eventsCtrl($scope, $state, $stateParams, snapRemote, UserService, Compa
   $scope.UserInterface = UserInterface;
   var
       ui = {title: 'Events',hasMenuIcon: true, hasDeleteIcon: false, hasBackIcon: false, hasMagnifierIcon: true, hasAddIcon: true, hasSaveIcon: false, hasCancelIcon: false, hasCloseIcon: false, showEventSubNav: true, hasCustomHomeClass: false, searching: false, AddIconState: "home.events.add",hasAddPhoto: false}
-    , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token }
-    , actions = { success: function(events, filters){
-                              // workaround for remove the non 'Active' events
-                              for(var i = 0, evt; evt = events[i++];) {
-                                if (evt.status != 'Active') {
-                                  events.splice(i-1, 1)
+      today = (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear()
+      future = "12/31/" + (new Date().getFullYear() + 10)
+    , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, start_date: today, end_date: future }
+    , actions = { success: function(events, filters) {
+                              // workaround for remove the non 'Active' and 'past' events
+                              var 
+                                  evt = []
+                                , today = new Date()
+                                , startDate = null
+                                , endDate = null
+                              for (var i = 0, len = events.length; i < len; i++) {
+                                startDate = new Date(events[i].start_date)
+                                endDate = new Date(events[i].end_date)
+                                if (events[i].status == 'Active' && (startDate >= today || endDate >= today)) {
+                                // if (events[i].status == 'Active') {
+                                  evt.push(events[i])
                                 }
                               }
-                              $scope.eventsItems = events
+                              $scope.eventsItems = evt
                               $scope.filters = filters
                               angular.extend(UserInterface, ui)
                             }
