10a6b1de97728f92317705cdaa9e7e43c7fe883f
flaviaza <flavia.ibanez@gmail.com>
2014-02-22 17:12:04 -0300

    fix layout because code was moved without advice

--- app/assets/js/controllers/events/eventsController.js
+++ app/assets/js/controllers/events/eventsController.js
@@ -14,21 +14,7 @@ function eventsCtrl($scope, $state, $stateParams, snapRemote, UserService, Compa
     , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, start_date: today, end_date: future, 'status[]': 'Active' }
     , options = { force: true }
     , actions = { success: function(events, filters) {
-                              // workaround for remove the non 'Active' and 'past' events
-                              var
-                                  evt = []
-                                , today = new Date()
-                                , startDate = null
-                                , endDate = null
-                              for (var i = 0, len = events.length; i < len; i++) {
-                                startDate = new Date(events[i].start_date)
-                                endDate = new Date(events[i].end_date)
-                                if (events[i].status == 'Active' && (startDate >= today || endDate >= today)) {
-                                // if (events[i].status == 'Active') {
-                                  evt.push(events[i])
-                                }
-                              }
-                              $scope.eventsItems = evt
+                              $scope.eventsItems = events
                               $scope.filters = filters
                               $scope.page = events.page
                               angular.extend(UserInterface, ui)
@@ -44,7 +30,7 @@ function eventsCtrl($scope, $state, $stateParams, snapRemote, UserService, Compa
     var
         today = (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear()
       , future = "12/31/" + (new Date().getFullYear() + 10)
-      , credentials = ($scope.event_status) ? { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, page: $scope.page, 'status[]': 'Active', 'event_status[]': status } 
+      , credentials = ($scope.event_status) ? { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, 'status[]': 'Active', 'event_status[]': status } 
                                             : { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, start_date: today, end_date: future, page: $scope.page, 'status[]': 'Active' }
       , options = { force: true }
       , actions = { success: function(events, filters) {
