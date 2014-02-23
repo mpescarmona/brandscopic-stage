b6791beb28e3d273b97df84b645b396cd6fa757b
flaviaza <flavia.ibanez@gmail.com>
2014-02-22 21:44:53 -0300

    fixes slider for photo gallery

--- app/assets/js/controllers/events/eventsPhotosController.js
+++ app/assets/js/controllers/events/eventsPhotosController.js
@@ -14,7 +14,7 @@ function eventsPhotosCtrl($scope, $state, $stateParams, snapRemote, UserService,
 
     var
         isPhoto = true
-      , ui = {hasMenuIcon: true, hasDeleteIcon: false, hasBackIcon: true, hasMagnifierIcon: false, hasAddIcon: false, hasSaveIcon: false, hasCancelIcon: false, hasCloseIcon: false, showEventSubNav: true, hasCustomHomeClass: false, searching: false, eventSubNav: "photos"}
+      , ui = {hasMenuIcon: false, hasDeleteIcon: false, hasBackIcon: true, hasMagnifierIcon: false, hasAddIcon: false, hasSaveIcon: false, hasCancelIcon: false, hasCloseIcon: false, showEventSubNav: true, hasCustomHomeClass: false, searching: false, eventSubNav: "photos"}
       , credentials = { company_id: CompanyService.getCompanyId(), auth_token: UserService.currentUser.auth_token, event_id: $stateParams.eventId }
       , actions = { success: function(event){
                                     $scope.event = event;
@@ -30,8 +30,10 @@ function eventsPhotosCtrl($scope, $state, $stateParams, snapRemote, UserService,
     Event.find(credentials, actions)
 
     $scope.$on('createPhoto', function (e, data) {
-      if ( data.render )
+      if ( data.render ){
+        $scope.hasPhotos = true
         $scope.photos.unshift({file_medium: data.src})
+      }
       else
         Photos.create(credentials, actions, data)
     })
