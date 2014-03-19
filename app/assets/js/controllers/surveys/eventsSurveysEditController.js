a3b077e74f3d2ea3b5452ac8b2ec2c89fb353f97
Alejandro Sayegh <alejandro@sayegh.com.ar>
2014-03-19 00:56:53 -0300

    Survey - Show Error message (partial) 	 Improvement in add/edit survey view

--- app/assets/js/controllers/surveys/eventsSurveysEditController.js
+++ app/assets/js/controllers/surveys/eventsSurveysEditController.js
@@ -3,58 +3,27 @@ function eventsSurveysEditController($scope, $state, $stateParams, $location,  s
       $state.go('login');
       return;
     }
+
+    // Inherit from base controller.
+    angular.extend(this, new eventsSurveysParentController($scope, surveysService));
+
     snapRemote.close()
     var ui = {hasMenuIcon: false, hasDeleteIcon: false, hasBackIcon: true, hasMagnifierIcon: false, hasAddIcon: false, hasSaveIcon: false, hasEditSurveyIcon: true, hasCancelIcon: false, hasCloseIcon: false, showEventSubNav: true, hasCustomHomeClass: false, searching: false, eventSubNav: "surveys",  AddIconState: "home.events.details.surveys.edit"}
     var survey_id = $stateParams.survey_id;
     angular.extend(UserInterface, ui)
     $scope.UserInterface = UserInterface;
 
-    $scope.gendersList = surveysService.genders;
-    $scope.racesList = surveysService.races;
-    $scope.agesList = surveysService.ages;
-    $scope.likehoodList = surveysService.likehood;
-
-    $scope.questions = {};
-    $scope.questions.brandsQuestion1 = [];
-    $scope.questions.brandsQuestion2 = [];
-    $scope.questions.brandsQuestion3 = [];
-    $scope.questions.brandsQuestion4 = [];
-
-
-    surveysService.getBrandslist().then( function (response){
-        $scope.brandslist = response;
-        angular.forEach(response, function (brand){
-            $scope.questions.brandsQuestion1.push({
-                id: 0 ,brand_id: brand.id, question_id: 1, name: brand.name, model: brand.id + "_model"
-            });
-            $scope.questions.brandsQuestion2.push({
-                id: 0, brand_id: brand.id, question_id: 2, name: brand.name, model: brand.id + "_model", input_id: "question2" + brand.id
-            });
-            $scope.questions.brandsQuestion3.push({
-                id: 0, brand_id: brand.id, question_id: 3, name: brand.name, model: brand.id + "_model", input_id: "question3" + brand.id
-            });
-            $scope.questions.brandsQuestion4.push({
-                id: 0, brand_id: brand.id, question_id: 4, name: brand.name, model: brand.id + "_model", input_id: "question4" + brand.id
-            });
-        });
-    }, function (response) {
-        console.log(response);
-    });
-
-    $scope.genderSelected = 10;
-
     function buildSurveyForEdit (surveys_answers) {
-
         angular.forEach(surveys_answers, function (answer) {
             switch(answer.kpi_id) {
               case 6:
-                  $scope.genderSelected = parseInt(answer.answer);
+                  $scope.genderModel.genderSelected = parseInt(answer.answer);
                   break;
               case 7:
-                  $scope.ageSelected = parseInt(answer.answer);
+                  $scope.ageModel.ageSelected = parseInt(answer.answer);
                   break;
               case 8:
-                  $scope.raceSelected = parseInt(answer.answer);
+                  $scope.raceModel.raceSelected = parseInt(answer.answer);
                   break;
             }
 
@@ -110,9 +79,9 @@ function eventsSurveysEditController($scope, $state, $stateParams, $location,  s
         },
         arrayAnswers = [];
 
-        arrayAnswers.push({ kpi_id: 6, answer: $scope.genderSelected });
-        arrayAnswers.push({ kpi_id: 7, answer: $scope.ageSelected });
-        arrayAnswers.push({ kpi_id: 8, answer: $scope.raceSelected });
+        arrayAnswers.push({ kpi_id: 6, answer: $scope.genderModel.genderSelected });
+        arrayAnswers.push({ kpi_id: 7, answer: $scope.ageModel.ageSelected });
+        arrayAnswers.push({ kpi_id: 8, answer: $scope.raceModel.raceSelected });
         angular.forEach($scope.questions, function (questions) {
             angular.forEach(questions, function (question) {
                arrayAnswers.push({ id: question.id, answer: question.model.toString() });
